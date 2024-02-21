ebf33fd56d58f4dfc7c5c56b23ec67ec431b9cf9
Antonio Jorge <141699267+AntonioJFJ@users.noreply.github.com>
2024-02-21 17:54:24 -0300

    Add files via upload

---
+++ ex003-index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Apresentação</title>
+</head>
+<body>
+    <h1>Bem-Vindo</h1>
+    <?php 
+        $nome = "Antonio";
+        $sobrenome = "Freitas";
+        const PAIS = "Brasil";
+        echo "Muito prazer, $nome $sobrenome! ", "Você mora no Brasil.";
+    ?>
+</body>
+</html>
